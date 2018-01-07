01975f728b17f26513e55ce4042fbe7f86cf2e52
jameswilddev <jameswilddev@outlook.com>
2018-01-07 15:21:49 +0000

    Non-animated scene node data is now exported.

--- tools/blender/sodiumSceneFile.py
+++ tools/blender/sodiumSceneFile.py
@@ -29,11 +29,37 @@ class ExportSodiumSceneFile(bpy.types.Operator, bpy_extras.io_utils.ExportHelper
     filename_ext = ".json"
 
     def execute(self, context):
+        scene_nodes = {}
+        for root_object in bpy.context.scene.objects:
+            if root_object.parent: continue
+            def recurse(object, collection):
+                exported = {
+                    "children": {}
+                }
+                collection[object.name] = exported
+
+                if object.type == "EMPTY":
+                    exported["type"] = "empty"
+                elif object.type == "LAMP":
+                    exported["type"] = "light"
+                elif object.type == "MESH":
+                    exported["type"] = "mesh"
+                    exported["mesh"] = object.data.name
+                else:
+                    self.report({"ERROR"}, "Object \"" + object.name + "\" is a(n) \"" + object.type + "\", which is not a supported type.")
+                    return False
+                if object.children:
+                    for child in object.children: 
+                        if not recurse(child, exported["children"]): return False
+                return True
+            if not recurse(root_object, scene_nodes): return {"FINISHED"}
+
         json_string = json.dumps({
             "framesPerSecond": {
                 "numerator": bpy.context.scene.render.fps,
                 "denominator": bpy.context.scene.render.fps_base
-            }
+            },
+            "sceneNodes": scene_nodes
         }, indent=4, sort_keys=True)
         file = open(self.properties.filepath, "w")
         file.write(json_string)
