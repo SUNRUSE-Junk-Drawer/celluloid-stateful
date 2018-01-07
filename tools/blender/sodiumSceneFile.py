894f464df27a93ca32104eaa6dcc05cbcb5669f1
jameswilddev <jameswilddev@outlook.com>
2018-01-07 14:27:43 +0000

    Added a blank script for Blender scene import/export.

---
+++ tools/blender/sodiumSceneFile.py
@@ -0,0 +1,43 @@
+bl_info = {
+    "name": "Sodium Scene File",
+    "category": "Import-Export"
+}
+
+import bpy
+
+class ImportSodiumSceneFile(bpy.types.Operator):
+    """Import Sodium Scene File"""
+    bl_idname = "import.sodiumscenefile"
+    bl_label = "Import Sodium Scene File (*.json)"
+
+    def execute(self, context):
+        return {"FINISHED"}
+
+class ExportSodiumSceneFile(bpy.types.Operator):
+    """Export Sodium Scene File"""
+    bl_idname = "export.sodiumscenefile"
+    bl_label = "Export Sodium Scene File (*.json)"
+
+    def execute(self, context):
+        return {"FINISHED"}
+
+def import_menu_func(self, context):
+    self.layout.operator(ImportSodiumSceneFile.bl_idname)
+
+def export_menu_func(self, context):
+    self.layout.operator(ExportSodiumSceneFile.bl_idname)
+
+def register():
+    bpy.utils.register_class(ImportSodiumSceneFile)
+    bpy.utils.register_class(ExportSodiumSceneFile)
+    bpy.types.INFO_MT_file_import.append(import_menu_func)
+    bpy.types.INFO_MT_file_export.append(export_menu_func)
+
+def unregister():
+    bpy.utils.unregister_class(ImportSodiumSceneFile)
+    bpy.utils.unregister_class(ExportSodiumSceneFile)
+    bpy.types.INFO_MT_file_import.remove(import_menu_func)
+    bpy.types.INFO_MT_file_export.remove(export_menu_func)
+
+if __name__ == "__main__":
+    register()
