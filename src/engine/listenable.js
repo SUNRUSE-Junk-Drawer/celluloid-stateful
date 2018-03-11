0111407dc77f895b1152d6dc08d30713d40ae470
jameswilddev <jameswilddev@outlook.com>
2018-03-11 07:46:19 +0000

    Added a listenable event class.

---
+++ src/engine/listenable.js
@@ -0,0 +1,42 @@
+export default class Listenable {
+  constructor() {
+    this.raising = false
+    this.eventsWhileRaising = []
+    this.callbacks = []
+  }
+
+  raise() {
+    if (this.raising) throw new Error("Listeners cannot be recursively raised")
+    this.raising = true
+    for (const callback of this.callbacks) callback()
+    this.raising = false
+    while (true) {
+      const event = this.eventsWhileRaising.shift()
+      if (!event) break
+      if (event.listen) {
+        this.listen(event.listen)
+      } else {
+        this.unlisten(event.unlisten)
+      }
+    }
+  }
+
+  listen(callback) {
+    if (this.raising) {
+      this.eventsWhileRaising.push({ listen: callback })
+    } else {
+      if (this.callbacks.indexOf(callback) != -1) throw new Error("Cannot add the same callback to a Listener twice")
+      this.callbacks.push(callback)
+    }
+  }
+
+  unlisten(callback) {
+    if (this.raising) {
+      this.eventsWhileRaising.push({ unlisten: callback })
+    } else {
+      const index = this.callbacks.indexOf(callback)
+      if (index == -1) throw new Error("Cannot remove a callback from a Listener which was never added")
+      this.callbacks.splice(index, 1)
+    }
+  }
+}
