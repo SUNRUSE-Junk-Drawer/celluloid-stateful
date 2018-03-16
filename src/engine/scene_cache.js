cc393facbc00d942ab53cafc44dc88eb0478d599
jameswilddev <jameswilddev@outlook.com>
2018-03-16 21:56:12 +0000

    Added a scene cache.

---
+++ src/engine/scene_cache.js
@@ -0,0 +1,30 @@
+import Cache from "./cache"
+import { readFile } from "fs"
+import { join } from "path"
+import onFileChange from "./on_file_change"
+import { parseScene } from "./scene"
+import FileParser from "./file_parser"
+
+class SceneCache extends Cache {
+  transform(key, then) {
+    readFile(join("data", key), (err, data) => {
+      if (err) {
+        console.error(`Failed to read "${key}" as binary for a scene`, err)
+        then(null)
+      } else {
+        let scene = null
+        try {
+          scene = parseScene(new FileParser(data.buffer))
+        } catch (e) {
+          console.error(`Failed to parse "${key}" as a scene`, e)
+        }
+        then(scene)
+      }
+    })
+  }
+}
+
+const cache = new SceneCache()
+onFileChange.listen(key => cache.changed(key))
+
+export default cache
