fd0021d5f7e498894450c017dd048c03869b39d8
jameswilddev <jameswilddev@outlook.com>
2018-03-17 10:34:14 +0000

    Removed unused imports.

--- src/engine/scene/scene.js
+++ src/engine/scene/scene.js
@@ -1,9 +1,3 @@
-import { parseNumberAnimation } from "./../animation/number_animation"
-import { parseMaterial } from "./material"
-import { parseMesh } from "./mesh"
-import { parseLamp } from "./lamp"
-import { parseCamera } from "./camera"
-import { parseNode } from "./node"
 import Disposable from "./../disposable"
 
 export class Scene extends Disposable {
