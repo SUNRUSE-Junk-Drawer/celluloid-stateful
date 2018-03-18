d15b29ac65818ce877a5571766ea2b20eba60b00
jameswilddev <jameswilddev@outlook.com>
2018-03-18 13:55:44 +0000

    Scene node instances now include a transform, which is computed on setting the frame.

--- src/engine/scene/meta_scene.js
+++ src/engine/scene/meta_scene.js
@@ -1,5 +1,6 @@
 import Disposable from "./../disposable"
 import sceneCache from "./scene_cache"
+import { vec3, mat4 } from "gl-matrix"
 
 export default class MetaScene extends Disposable {
   constructor(mainScenePath) {
@@ -31,6 +32,7 @@ class SceneInstance extends Disposable {
     this.metaScene = metaScene
     this.dropCurrentInstance()
     this.frame = 0
+    this.transform = mat4.create()
     this.sceneHandle = sceneCache.createHandle(path, scene => {
       if (scene == null) {
         this.dropCurrentInstance()
@@ -50,7 +52,10 @@ class SceneInstance extends Disposable {
   setFrame(frame) {
     this.checkNotDisposed()
     this.frame = frame
-    for (const name in this.dataInstances) this.dataInstances[name].setFrame(frame)
+    if (this.dataInstances) {
+      for (const name in this.dataInstances) this.dataInstances[name].setFrame(frame)
+      for (const name in this.rootNodeInstances) this.rootNodeInstances[name].setFrame(frame)
+    }
   }
 
   render() {
@@ -75,6 +80,8 @@ class SceneInstance extends Disposable {
   }
 }
 
+const scale = vec3.create()
+
 class NodeInstance {
   constructor(sceneInstance, parent, node) {
     this.parent = parent
@@ -86,11 +93,33 @@ class NodeInstance {
       this.dataInstance = null
     }
 
+    this.transform = mat4.create()
+
     this.nodeInstances = {}
     for (const name in node.nodes) new NodeInstance(sceneInstance, this, node.nodes[name])
+
+    this.setFrame(sceneInstance.frame)
 
     sceneInstance.nodeInstances[node.name] = this
     const parentCollection = parent == sceneInstance ? parent.rootNodeInstances : parent.nodeInstances
     parentCollection[node.name] = this
+  }
+
+  setFrame(frame) {
+    scale[0] = this.node.scale[0].sample(frame)
+    scale[1] = this.node.scale[1].sample(frame)
+    scale[2] = this.node.scale[2].sample(frame)
+    mat4.fromScaling(this.transform, scale)
+
+    mat4.rotateX(this.transform, this.transform, this.node.rotation[0].sample(frame))
+    mat4.rotateY(this.transform, this.transform, this.node.rotation[1].sample(frame))
+    mat4.rotateZ(this.transform, this.transform, this.node.rotation[2].sample(frame))
+
+    this.transform[12] = this.node.translation[0].sample(frame)
+    this.transform[13] = this.node.translation[1].sample(frame)
+    this.transform[14] = this.node.translation[2].sample(frame)
+
+    mat4.multiply(this.transform, this.parent.transform, this.transform)
+    for (const name in this.nodeInstances) this.nodeInstances[name].setFrame(frame)
   }
 }
