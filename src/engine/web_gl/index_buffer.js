6ef43248e4a866bfe16da3cb768f3a8789331afa
jameswilddev <jameswilddev@outlook.com>
2018-03-19 20:47:30 +0000

    Added a draw() method to IndexBuffer.

--- src/engine/web_gl/index_buffer.js
+++ src/engine/web_gl/index_buffer.js
@@ -1,11 +1,18 @@
 import Buffer from "./buffer"
+import context from "./../context"
 
 export default class IndexBuffer extends Buffer {
   constructor(indices) {
     super(Math.max.apply(null, indices) < 256 ? new Uint8Array(indices) : new Uint16Array(indices))
+    this.usesBytes = Math.max.apply(null, indices) < 256
   }
 
   target(gl) {
     return gl.ELEMENT_ARRAY_BUFFER
+  }
+
+  draw() {
+    this.bind()
+    context.gl.drawElements(context.gl.TRIANGLES, this.data.length, this.usesBytes ? context.gl.UNSIGNED_BYTE : context.gl.UNSIGNED_SHORT, 0)
   }
 }
